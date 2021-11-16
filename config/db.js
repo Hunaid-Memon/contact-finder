788c41d7b454d8295e5def22a2ac64cd9539380d
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-11-17 00:59:34 +0500

    connect mongoDB

--- config/db.js
+++ config/db.js
@@ -3,16 +3,28 @@ const config = require('config');
 
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false
-    }).then(() => console.log('MongoDB is connected'))
-    .catch(err => {
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+        console.log('connected to MongoDB');
+    } catch (err) {
         console.error(err.message);
         process.exit(1)
-    });
+    }
 }
+
+// const connectDB = () => {
+//     mongoose.connect(db, {
+//         useNewUrlParser: true,
+//         // useCreateIndex: true,
+//         // useFindAndModify: false
+//     }).then(() => console.log('MongoDB is connected'))
+//     .catch(err => {
+//         console.error(err.message);
+//         process.exit(1)
+//     });
+// }
 
 module.exports = connectDB;
